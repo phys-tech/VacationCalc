9ce14eccd676f530932c72dbaf6cbae3551ae8dc
Ilya Denisov <ilya.denisov@fastdev.se>
2016-10-05 12:36:01 +0500

    [ADD] Save and load holiday's dates to xml file.

--- VacationCalc/VacationCalc/UI/HolidaysForm.cs
+++ VacationCalc/VacationCalc/UI/HolidaysForm.cs
@@ -7,33 +7,70 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using Telerik.WinControls;
+using System.Xml.Linq;
+using System.IO;
 
 namespace VacationCalc.UI
 {
     public partial class HolidaysForm : Telerik.WinControls.UI.RadForm
     {
-        private List<DateTime> dateList;
+        private IEnumerable<DateTime> sortedDates;
+        private string XmlFilename = "Holidays.xml";
+
         public HolidaysForm()
         {
             InitializeComponent();
-            dateList = new List<DateTime>();
+            LoadDatesFromXML();
+            UpdateDatesOnLabel();
         }
 
-        private void calendarHolidays_SelectionChanged(object sender, EventArgs e)
+        private void UpdateDatesOnLabel()
         {
             labelSelectedDates.Text = "";
-            IEnumerable<DateTime> stuff = calendarHolidays.SelectedDates;
-            var sorted = stuff.OrderBy(u => u.Date);
-            foreach (DateTime date in sorted)
+            foreach (DateTime date in sortedDates)
             {
                 labelSelectedDates.Text += date.ToLongDateString();
                 labelSelectedDates.Text += "\n";
-            }
+            }        
+        }
+
+        private void calendarHolidays_SelectionChanged(object sender, EventArgs e)
+        {            
+            sortedDates = calendarHolidays.SelectedDates;
+            sortedDates = sortedDates.OrderBy(u => u.Date);
+            UpdateDatesOnLabel();
         }
 
         private void bOK_Click(object sender, EventArgs e)
         {
+            SaveDatesToXML();
             this.Close();
+        }
+
+        private void SaveDatesToXML()
+        {
+            XDocument doc = new XDocument(new XElement("Root"));
+            foreach (DateTime date in sortedDates)
+            {
+                XElement element = new XElement("Holiday",
+                                    new XElement("Date", date.Date));
+                doc.Root.Add(element);
+            }
+            doc.Save(XmlFilename);
+        }
+
+        private void LoadDatesFromXML()
+        {
+            XDocument doc = XDocument.Load(XmlFilename);            
+            List<DateTime> tempList = new List<DateTime>();
+            foreach (XElement element in doc.Root.Elements())
+            { 
+                DateTime date = DateTime.Parse(element.Value);
+                tempList.Add(date);
+            }
+            sortedDates = tempList;
+            sortedDates = sortedDates.OrderBy(u => u.Date);
+            calendarHolidays.SelectedDates.AddRange(tempList.ToArray());
         }
 
 
